0e07538a2b0089edf44f1450e1d90f438e8ea3f9
rishad <71107992+rishadpt0@users.noreply.github.com>
2020-09-11 11:06:29 +0530

    Update sample_config.py

--- metabutler/sample_config.py
+++ metabutler/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1365900902:AAE5UvxO6TXnxDQQX3Q2j_62L9VWTNMkxYU"
+    OWNER_ID = "1100580536"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "rizuppt"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
